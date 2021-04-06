874134383bd0365eef175b65efc2654879672dff
callie rogers <callie1013@gmail.com>
2021-04-06 20:20:18 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,9 +3,9 @@ def turn_count(board)
   board.each do |spaces|
     if spaces == "X" || spaces == "O"
       counter += 1
-    end
+end
     counter
-  end
+end
 
   def current_player(board)
     turn_count(board) % 2 == 0? "X" : "O"
